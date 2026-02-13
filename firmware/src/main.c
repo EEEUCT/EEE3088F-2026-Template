06de60f7f00d9a4fd359409bcb53488cca42f869
Fred Nicolls <30141109+nicollsf@users.noreply.github.com>
2026-02-13 11:04:35 +0200

    Initial template structure for EEE3088F

---
+++ firmware/src/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("EEE3088F: Firmware System Initialized\n");
+    return 0;
+}
---
+++ hardware/README.txt
@@ -0,0 +1,1 @@
+Please save your KiCad project files and your M2_Gerbers.zip file in this directory
