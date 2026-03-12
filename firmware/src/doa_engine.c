747a85f2ae4b3c7afe1c1573179e886f59c9cc63
Fred Nicolls <30141109+nicollsf@users.noreply.github.com>
2026-03-12 21:32:51 +0200

    More work on solution

--- firmware/src/doa_engine.c
+++ firmware/src/doa_engine.c
@@ -9,7 +9,14 @@
 int8_t calculate_doa_angle_2mic(const int16_t* mic_left_data, const int16_t* mic_right_data, size_t N, float d_mm, uint32_t fs) {
     
     // TODO: Implement cross-correlation to find time lag
+    // HINT: Limit your search window (max_lag) to the physical max delay possible.
+    //       Searching too far might find "aliased" peaks if the signal is periodic!
+    
     // TODO: Convert lag to angle based on microphone spacing (d_mm)
+    // HINT: The argument to asin() must be between -1.0 and 1.0.
+    //       Due to noise or rounding, your calculation might produce 1.001.
+    //       Clamp this value before calling asin() to avoid NaN errors.
+    
     // TODO: Return angle in degrees (-90 to 90)
 
     return 0; // Default return to allow compilation
